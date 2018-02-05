3124cce906a8f41cefa4df8bac73c01d0806c892
Falzed <oskari_kulmala@hotmail.com>
2018-02-05 18:43:29 +0200

    poistettu turhat kansiot

--- osa-2-15/src/components/Person.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const Person = ({person, removePerson}) => {
-    return(<tr><td>{person.name}</td><td>{person.number}</td><td>
-        <button onClick={() => removePerson(person)}>Poista</button></td></tr>)
-}
-
-export default Person
